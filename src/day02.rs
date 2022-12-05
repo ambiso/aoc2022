1c54a1c1f65977ba15fcbf15a595c33d30246d10
ambiso <ambiso@invalid>
2022-12-05 13:52:29 +0100

    Simd variant

--- src/day02.rs
+++ src/day02.rs
@@ -1,10 +1,10 @@
+use crate::error::Result;
+use crate::util::read_string;
+use rand::distributions::Uniform;
+use rand::Rng;
 use std::arch::x86_64::{__m256i, _mm256_shuffle_epi8};
 use std::fs;
 use std::simd::{u8x32, SimdUint};
-use crate::error::Result;
-use rand::distributions::Uniform;
-use rand::Rng;
-use crate::util::read_string;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 enum RPS {
@@ -93,15 +93,17 @@ pub fn solve_b() -> Result<i64> {
         } else {
             unreachable!()
         };
-        score += match outcome {
+        let s = match outcome {
             Outcome::Win => 6,
             Outcome::Draw => 3,
             Outcome::Lose => 0,
         } + what_score(mine);
+        score += s;
     }
     Ok(score)
 }
 
+#[allow(unused)]
 pub fn gen_lut() {
     for other_char in ['A', 'B', 'C'] {
         for outcome_char in ['X', 'Y', 'Z'] {
@@ -161,6 +163,7 @@ pub fn solve_a() -> Result<()> {
     Ok(())
 }
 
+#[allow(unused)]
 pub fn gen_input_day2() -> Result<()> {
     let mut s = Vec::<u8>::new();
 
@@ -186,27 +189,53 @@ pub fn solve_b_opt() -> Result<i64> {
     let mut score = 0i64;
     let v = fs::read("inputs/day02g")?;
     let lut = [
-        2, 3, 7, 0, 4, 8, 1, 5, 6, 0, 0, 0, 0, 0, 0, 0,
-        2, 3, 7, 0, 4, 8, 1, 5, 6, 0, 0, 0, 0, 0, 0, 0,
+        2, 3, 7, 0, 4, 8, 1, 5, 6, 0, 0, 0, 0, 0, 0, 0, 2, 3, 7, 0, 4, 8, 1, 5, 6, 0, 0, 0, 0, 0,
+        0, 0,
     ];
     let simd_lut = __m256i::from(u8x32::from(lut));
-    for l in 0..v.len() / 4 / 32 {
-        let mut d1s = [0; 32];
-        let mut d2s = [0; 32];
-        for i in 0..32 {
-            d1s[i] = v[l * 4 * 32 + 4 * i] - 'A' as u8;
-            d2s[i] = v[l * 4 * 32 + 2 + 4 * i] - 'X' as u8;
-        }
-        let vals = u8x32::from(d1s) * u8x32::splat(3) + u8x32::from(d2s);
+    let m_d1 = u8x32::from([
+        0xff, 0x00, 0x00, 0x00, 0xff, 0x00, 0x00, 0x00, 0xff, 0x00, 0x00, 0x00, 0xff, 0x00, 0x00,
+        0x00, 0xff, 0x00, 0x00, 0x00, 0xff, 0x00, 0x00, 0x00, 0xff, 0x00, 0x00, 0x00, 0xff, 0x00,
+        0x00, 0x00,
+    ]);
+    let m_d2 = m_d1.rotate_lanes_right::<2>();
+    let mut chunks = v.chunks_exact(32 * 4);
+    for l in &mut chunks {
+        let l1 = u8x32::from_slice(&l[32 * 0..32 * 1]);
+        let l2 = u8x32::from_slice(&l[32 * 1..32 * 2]);
+        let l3 = u8x32::from_slice(&l[32 * 2..32 * 3]);
+        let l4 = u8x32::from_slice(&l[32 * 3..32 * 4]);
+
+        let l1_d1 = l1 & m_d1;
+        let l2_d1 = l2 & m_d1;
+        let l3_d1 = l3 & m_d1;
+        let l4_d1 = l4 & m_d1;
+
+        let l1_d2 = l1 & m_d2;
+        let l2_d2 = l2 & m_d2;
+        let l3_d2 = l3 & m_d2;
+        let l4_d2 = l4 & m_d2;
+
+        let d1s = l1_d1
+            | l2_d1.rotate_lanes_right::<1>()
+            | l3_d1.rotate_lanes_right::<2>()
+            | l4_d1.rotate_lanes_right::<3>();
+        let d2s = l1_d2.rotate_lanes_left::<2>()
+            | l2_d2.rotate_lanes_left::<1>()
+            | l3_d2
+            | l4_d2.rotate_lanes_right::<1>();
+        let vals = (u8x32::from(d1s) - u8x32::splat('A' as u8)) * u8x32::splat(3)
+            + (u8x32::from(d2s) - u8x32::splat('X' as u8));
         let vals = __m256i::from(vals);
         let scores = unsafe { _mm256_shuffle_epi8(simd_lut, vals) };
-        score += u8x32::from(scores).reduce_sum() as i64;
+        // dbg!(u8x32::from(simd_lut), u8x32::from(vals), u8x32::from(scores));
+        score += u8x32::from(scores).reduce_sum() as i64 + 32;
     }
-    let r = v.len() / 4 / 32 * 32 + 1..v.len() / 4;
-    for l in r.clone() {
-        let other = v[l * 4] - 'A' as u8;
-        let desired_outcome = v[l * 4 + 2] - 'X' as u8;
-        score += lut[(other * 3 + desired_outcome) as usize] as i64;
+    let rem = chunks.remainder();
+    for l in 0..rem.len() / 4 {
+        let d1 = rem[l * 4] - 'A' as u8;
+        let d2 = rem[l * 4 + 2] - 'X' as u8;
+        score += lut[(d1 * 3 + d2) as usize] as i64 + 1;
     }
-    Ok(score + v.len() as i64 / 4)
+    Ok(score)
 }
