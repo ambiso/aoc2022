c4b2fd5c427b49d055246d1e539209e7d6398554
ambiso <ambiso@invalid>
2022-12-17 20:15:23 +0100

    Day 16 part 2 attempt

--- src/day16.rs
+++ src/day16.rs
@@ -14,7 +14,7 @@ fn solve_internal(
     time_left: i64,
     node_values: Vec<i64>,
 ) -> i64 {
-    if time_left == 0 {
+    if time_left == 0 || node_values.iter().all(|x| *x == 0) {
         return 0;
     }
     if cache.contains_key(&(node, time_left, node_values.clone())) {
@@ -81,8 +81,103 @@ pub fn solve_a() -> Result<i64> {
     Ok(s)
 }
 
+fn solve_for_b(graph: Vec<Vec<usize>>, node_values: Vec<i64>, node: usize) -> i64 {
+    let mut cache = HashMap::new();
+    solve_internal_b(&mut cache, &graph, (node, node), 26, node_values)
+}
+
+fn solve_internal_b(
+    cache: &mut HashMap<((usize, usize), i64, Vec<i64>), i64>,
+    graph: &Vec<Vec<usize>>,
+    node: (usize, usize),
+    time_left: i64,
+    node_values: Vec<i64>,
+) -> i64 {
+    if time_left == 0 || node_values.iter().all(|x| *x == 0) {
+        return 0;
+    }
+    if cache.contains_key(&(node, time_left, node_values.clone())) {
+        cache[&(node, time_left, node_values)]
+    } else {
+        // substructure: visit neighbors with 1 less time, or open valve of current node
+        let mut max_val = 0;
+        let mut node_values_open_0 = node_values.clone();
+        node_values_open_0[node.0] = 0;
+
+        let mut node_values_open_1 = node_values.clone();
+        node_values_open_1[node.1] = 0;
+
+        let mut node_values_open_both = node_values.clone();
+        node_values_open_both[node.0] = 0;
+        node_values_open_both[node.1] = 0;
+
+        // 0 opens, 1 travels
+        if node_values[node.0] != 0 {
+            for neigh in &graph[node.1] {
+                // move to that node and solve subproblem
+                max_val = max_val.max(
+                    solve_internal_b(
+                        cache,
+                        graph,
+                        (node.0, *neigh),
+                        time_left - 1,
+                        node_values_open_0.clone(),
+                    ) + node_values[node.0] * (time_left - 1),
+                );
+            }
+        }
+
+        // 0 travels, 1 opens
+        if node_values[node.1] != 0 {
+            for neigh in &graph[node.0] {
+                // move to that node and solve subproblem
+                max_val = max_val.max(
+                    solve_internal_b(
+                        cache,
+                        graph,
+                        (*neigh, node.1),
+                        time_left - 1,
+                        node_values_open_1.clone(),
+                    ) + node_values[node.1] * (time_left - 1),
+                );
+            }
+        }
+        // both open
+        if node.0 != node.1 && node_values[node.0] != 0 && node_values[node.1] != 0 {
+            max_val = max_val.max(
+                solve_internal_b(
+                    cache,
+                    graph,
+                    node,
+                    time_left - 1,
+                    node_values_open_both.clone(),
+                ) + node_values[node.1] * (time_left - 1)
+                    + node_values[node.0] * (time_left - 1),
+            );
+        }
+
+        // both travel
+        for neigh0 in &graph[node.0] {
+            for neigh1 in &graph[node.1] {
+                max_val = max_val.max(solve_internal_b(
+                    cache,
+                    graph,
+                    (*neigh0, *neigh1),
+                    time_left - 1,
+                    node_values.clone(),
+                ));
+            }
+        }
+
+        cache.insert((node, time_left, node_values), max_val);
+        max_val
+    }
+}
+
 pub fn solve_b() -> Result<i64> {
-    Ok(0)
+    let (graph, node_values, name_map) = parse_input()?;
+    let s = solve_for_b(graph, node_values, name_map[&"AA".to_string()]);
+    Ok(s)
 }
 
 #[cfg(test)]
@@ -91,8 +186,7 @@ mod test {
 
     #[test]
     fn test_a() {
-        assert!(solve_a().unwrap() < 2173);
-        // assert_eq!(solve_a().unwrap(), 5256611);
+        assert_eq!(solve_a().unwrap(), 1751);
     }
 
     #[test]
